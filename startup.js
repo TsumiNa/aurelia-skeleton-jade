e9fabc3fb883214f222c9d636506ad018ba53dba
tsumina <f0230074@gmail.com>
2015-11-27 18:41:44 +0900

    update

--- startup.js
+++ startup.js
@@ -1,14 +1,1 @@
-if (Meteor.isServer) {
-    Meteor.startup(function() {
-        System.import('collections')
-    });
-}
-
-if (Meteor.isClient) {
-    Meteor.startup(function() {
-        System.import('collections')
-            .then(function() {
-                System.import('aurelia-bootstrapper')
-            })
-    });
-}
+System.import('collections')
